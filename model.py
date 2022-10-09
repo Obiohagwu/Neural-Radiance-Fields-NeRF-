59ee82faec9b1aab61863c2d4e3a8e9ae70e476e
Obi Ohagwu <obiohagwu8@gmail.com>
2022-10-09 17:26:40 -0400

    model finish have to test

--- model.py
+++ model.py
@@ -31,10 +31,37 @@ class NeRF(torch.nn.Module):
                     self.layers_xyz.append(torch.nn.Linear(self.dim_xyz+256, 256))
                 else:
                     self.layers_xyz.append(torch.nn.Linear(256,256))
-            self.fc
+            self.fc_feat = torch.nn.Linear(256, 256)
+            self.fc_alpha = torch.nn.Linear(256, 1)
+
+            self.layers_dir = torch.nn.ModuleList()
+            self.layers_dir.append(torch.nn.Linear(256 + self.dim_dir, 128))
+            for i in range(3):
+                self.layers_dir.append(torch.nn.Linear(128, 128))
+            self.fc_rgb = torch.nn.Linear(128, 3)
+            self.relu = torch.nn.functional.relu
         
-        pass 
-    pass
+    def forward(self, x):
+            xyz, dirs = x[..., : self.dim_xyz], x[..., self.dim_xyz :]
+            for i in range(8):
+                if i == 4:
+                    x = self.layers_xyz[i](torch.cat((xyz, x), -1))
+                else:
+                    x = self.layers_xyz[i](x)
+                x = self.relu(x)
+            feat = self.fc_feat(x)
+            alpha = self.fc_alpha(feat)
+            if self.use_viewdirs:
+                x = self.layers_dir[0](torch.cat((feat, dirs), -1))
+            else:
+                x = self.layers_dir[0](feat)
+            x = self.relu(x)
+            for i in range(1, 3):
+                x = self.layers_dir[i](x)
+                x = self.relu(x)
+            rgb = self.fc_rgb(x)
+            return torch.cat((rgb, alpha), dim=-1)
+       
 
 def test():
     print("Testing!")
