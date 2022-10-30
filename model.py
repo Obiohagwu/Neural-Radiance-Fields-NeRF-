545b85b0c9c0970b39aaf865aba98faf1a67dc0d
Obi Ohagwu <obiohagwu8@gmail.com>
2022-10-30 04:44:44 -0400

    model edit

--- model.py
+++ model.py
@@ -3,65 +3,92 @@ import torch
 
 # ((x,y,z), theta, phi) ---> ((r,g,b), sigma(density))
 
-class NeRF(torch.nn.Module):
-    def __init__(
-        self,
-        num_layers=8,
-        hidden_size=256,
-        skip_connections=4,
-        num_encoding_func_xyz=6,
-        num_encoding_func_dir=4,
-        include_input_xyz=True,
-        include_input_dir=True,
-        ):
-            super(NeRF, self).__init__()
-            if include_input_xyz:
-                include_input_xyz = 3
-            else:
-                include_input_xyz = 0
-            self.dim_xyz = include_input_xyz+2*3*num_encoding_func_xyz
-            self.dim_dir = include_input_dir+2*3*num_encoding_func_dir
-
-            self.layers_xyz = torch.nn.ModuleList()
-            self.use_viewdirs = use_viewdirs
-            self.layers_xyz.append(torch.nn.Linear(self.dim_xyz+256, 256))
-            #iterate through layers
-            for i in range(1,8): # traverse through 8 layers
-                if i == 4: # if idx = 4
-                    self.layers_xyz.append(torch.nn.Linear(self.dim_xyz+256, 256))
-                else:
-                    self.layers_xyz.append(torch.nn.Linear(256,256))
-            self.fc_feat = torch.nn.Linear(256, 256)
-            self.fc_alpha = torch.nn.Linear(256, 1)
-
-            self.layers_dir = torch.nn.ModuleList()
-            self.layers_dir.append(torch.nn.Linear(256 + self.dim_dir, 128))
-            for i in range(3):
-                self.layers_dir.append(torch.nn.Linear(128, 128))
-            self.fc_rgb = torch.nn.Linear(128, 3)
-            self.relu = torch.nn.functional.relu
+class MLP(nn.Module):
+    def __init__(self, network_depth, network_width, network_skip, use_viewdirs, use_disp, use_noise):
+        super(MLP, self).__init__()
+        
+        self.use_viewdirs = use_viewdirs
+        self.use_disp = use_disp
+        self.use_noise = use_noise
+        
+        # Set network
+        self.network = nn.Sequential()
+        self.network.add_module('layer_0', nn.Linear(3 + 3 * use_viewdirs, network_width))
+        self.network.add_module('activation_0', nn.ReLU())
+        
+        for i in range(1, network_depth):
+            self.network.add_module('layer_{}'.format(i), nn.Linear(network_width + network_skip * i, network_width))
+            self.network.add_module('activation_{}'.format(i), nn.ReLU())
+        
+        self.network.add_module('layer_{}'.format(network_depth), nn.Linear(network_width + network_skip * network_depth, 4 + use_disp))
+        
+        # Set noise
+        if self.use_noise:
+            self.noise = nn.Parameter(torch.zeros(1))
         
     def forward(self, x):
-            xyz, dirs = x[..., : self.dim_xyz], x[..., self.dim_xyz :]
-            for i in range(8):
-                if i == 4:
-                    x = self.layers_xyz[i](torch.cat((xyz, x), -1))
-                else:
-                    x = self.layers_xyz[i](x)
-                x = self.relu(x)
-            feat = self.fc_feat(x)
-            alpha = self.fc_alpha(feat)
-            if self.use_viewdirs:
-                x = self.layers_dir[0](torch.cat((feat, dirs), -1))
-            else:
-                x = self.layers_dir[0](feat)
-            x = self.relu(x)
-            for i in range(1, 3):
-                x = self.layers_dir[i](x)
-                x = self.relu(x)
-            rgb = self.fc_rgb(x)
-            return torch.cat((rgb, alpha), dim=-1)
-       
+        if self.use_noise:
+            x = x + self.noise * torch.randn_like(x)
+        
+        return self.network(x)
+
+
+class NeRF(nn.Module):
+    def __init__(self, network_depth, network_width, network_skip, use_viewdirs, use_disp, use_noise):
+        super(NeRF, self).__init__()
+        
+        self.use_viewdirs = use_viewdirs
+        self.use_disp = use_disp
+        self.use_noise = use_noise
+        
+        # Set network
+        self.network = MLP(network_depth, network_width, network_skip, use_viewdirs, use_disp, use_noise)
+        
+        # Set noise
+        if self.use_noise:
+            self.noise = nn.Parameter(torch.zeros(1))
+        
+    def forward(self, x, viewdirs):
+        if self.use_noise:
+            x = x + self.noise * torch.randn_like(x)
+        
+        if self.use_viewdirs:
+            x = torch.cat([x, viewdirs], -1)
+        
+        return self.network(x)
+
+    def get_rays(self, H, W, focal, c2w):
+        i, j = torch.meshgrid(torch.arange(H), torch.arange(W))
+        i = i.float()
+        j = j.float()
+        dirs = torch.stack([(i - H * 0.5) / focal, -(j - W * 0.5) / focal, -torch.ones_like(i)], -1)
+        rays_d = dirs.view(H * W, 3)
+        rays_o = torch.zeros_like(rays_d)
+        rays_d = normalize_rays(rays_d, c2w)
+        rays_o = normalize_rays(rays_o, c2w)
+        return rays_o, rays_d
+
+    def render_rays(self, rays_o, rays_d, near, far, N_samples, rand=True, retraw=False, white_bkgd=False):
+        batch_size = rays_o.shape[0]
+        z_vals = torch.linspace(near, far, N_samples)
+        z_vals = z_vals.view(1, 1, N_samples).repeat(batch_size, 1, 1)
+        
+        if rand:
+            z_vals = z_vals + torch.rand_like(z_vals) * (z_vals[:, 1:] - z_vals[:, :-1])
+        
+        z_vals = z_vals.view(batch_size, -1)
+        rays_o = rays_o[:, None, :].repeat(1, N_samples, 1)
+        rays_d = rays_d[:, None, :].repeat(1, N_samples, 1)
+        rays_o = rays_o.view(batch_size, -1, 3)
+        rays_d = rays_d.view(batch_size, -1, 3)
+
+
+
+
+
+
+
+
 
 def test():
     print("Testing!")
